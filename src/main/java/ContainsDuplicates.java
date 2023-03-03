19d72a5fe968358e0825940057cbe88bd949a79d
jarthurj <jaoldaker1@gmail.com>
2023-03-03 20:43:52 +0000

    COMPLETE

--- src/main/java/ContainsDuplicates.java
+++ src/main/java/ContainsDuplicates.java
@@ -1,13 +1,20 @@
-
+import java.util.HashSet;
+import java.util.Set;
 
 public class ContainsDuplicates {
     /**
-     * Determine if an array of items contains any duplicate values. You should use a Set for this: remember that
-     * Sets can not have duplicate values, but you may check if a value is already contained using the .contains method.
+     * Determine if an array of items contains any duplicate values. You should use a Set for this: rememb Sets can not have duplicate values, but you may check if a value is already container that
+     *ed using the .contains method.
      * @param nums an array of ints.
      * @return true if nums contains any duplicate values, false if it does not.
      */
     public boolean containsDuplicate(int[] nums){
+        Set<Integer> hasDuplicates = new HashSet<Integer>();
+        for(int num:nums){
+            if(!hasDuplicates.add(num)){
+                return true;
+            };
+        }
         return false;
     }
 }
